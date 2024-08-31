deeab971ea6e9005fd749c32e16018d8e209e1ed
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-30 19:29:52 -0700

    Setup DB connection

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // var createError = require('http-errors');
 var express = require('express');
-// var path = require('path');
+var path = require('path');
 // var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 const cors = require('cors');
@@ -8,8 +8,12 @@ require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+require('dotenv').config();
+const pool = require('./server/db');
+var app = express();
+const PORT = process.env.PORT_NUMBER || 3001;
 
-const PORT = process.env.PORT || 3001;
+
 //change for deployment
 app.use(
   cors(
@@ -18,13 +22,21 @@ app.use(
       credentials: true
     }
   )
-)
-var app = express();
+);
+
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
-
+app.get('/test',async (req, res) => {
+  try{
+    const device = await pool.query('SELECT * FROM device;');
+    console.log(device.rows);
+    res.json(device.rows);
+  }catch (e) {
+    console.log(e.message);
+  }
+});
 // app.use(logger('dev'));
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
@@ -33,6 +45,8 @@ var app = express();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
