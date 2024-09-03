60c10d70683ceb4861e05b7d08f865b5778ee8e9
MatthewCCChang <deadwalker1228@gmail.com>
2024-09-03 11:18:59 -0700

    Device routes halfway done and tested

--- routes/device.js
+++ routes/device.js
@@ -6,9 +6,9 @@ deviceRouter.use(express.json());
 //create new device
 deviceRouter.post('/new_device', async(req, res) => {
   try{
-    const {long, lat, last, region, } = req.body;
+    const {long, lat, last, region } = req.body;
     const deviceData = [long, lat, last, region];
-    const postQuery = `INSERT INTO device(longitude, latitude, last_refill, region) VALUES($1,2,3,4];)`;
+    const postQuery = `INSERT INTO device(longitude, latitude, last_refill, region) VALUES($1,$2,$3,$4);`;
     const status = await pool.query(postQuery, deviceData);
     res.status(200).json(status);
   }
@@ -21,21 +21,30 @@ deviceRouter.post('/new_device', async(req, res) => {
 deviceRouter.get('/all_device', async(req, res) => {
   try{
     const data = await pool.query(`SELECT * FROM device;`);
-    res.json(data);
+    res.json(data.rows);
   }catch(e){
     res.status(500).send(e.message);
   }
 })
 
 //get specific device
-deviceRouter.get('/dashboard/:id', async(req, res) => {
+deviceRouter.get('/device/:id', async(req, res) => {
   try{
-    const deviceId = req.params;
+    const deviceId = req.params.id;
+    console.log(deviceId);
     const data = await pool.query(`SELECT * FROM device WHERE id=$1;`,[deviceId]);
-    res.json(data);
+    res.status(200).json(data.rows);
   }catch(e){
     res.status(500).send(e.message);
   }
 })
+
+//update last_refill date
+
+//update refills count
+
+//update capacity
+
+//update longitude and latitude
 
 module.exports = deviceRouter;
