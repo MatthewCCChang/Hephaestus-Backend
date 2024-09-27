d7510b8da30474658b021bdf692ba416b88b58bc
Matthew <deadwalker1228@gmail.com>
2024-09-27 12:58:03 -0700

    Added firebase-config file and verified middleware

--- server/firebase.js
+++ server/firebase.js
@@ -1,16 +1,8 @@
-const admin = require('firebase-admin');
-// const serviceAccount = require(''); //path to service json
-const serviceAccount = {
-    apiKey: "AIzaSyDp36G5oh-otAKLU_r_DuN8zDYp_WV1PFI",
-    authDomain: "hephaestus-7ec3a.firebaseapp.com",
-    projectId: "hephaestus-7ec3a",
-    storageBucket: "hephaestus-7ec3a.appspot.com",
-    messagingSenderId: "132893027702",
-    appId: "1:132893027702:web:f2ef92e1f1cc20ec539e57"
-  };
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-adminsdk.json"); //path to service json
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
